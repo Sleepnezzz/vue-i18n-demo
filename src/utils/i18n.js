f42635e6bde6616065ed21030f3419fa1e1d809a
thagoon uppathi <bbie.eng@gmail.com>
2020-07-15 23:57:04 +0700

    initial commit

---
+++ src/utils/i18n.js
@@ -0,0 +1,5 @@
+const messages = {
+  en: { msg: { me: "I", love: "love", you: "you" } },
+  th: { msg: { me: "ฉัน", love: "รัก", you: "คุณ" } },
+  north: { msg: { me: "เปิ้น", love: "ฮัก", you: "ตั๋ว" } },
+};
